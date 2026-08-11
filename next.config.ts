0394d47380f9c852cc46aa5075a9784e3abdf8df
raymundoht <raymundoherrera@MacBook-Pro-de-Raymundo.local>
2026-08-11 16:01:17 -0600

    chore: limpieza de archivos no utilizados y corrección de pdfkit en next.js

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  serverExternalPackages: ["pdfkit"],
   poweredByHeader: false,
   outputFileTracingRoot: process.cwd(),
   images: {
